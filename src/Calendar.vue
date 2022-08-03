619a76e801e2907ee96f9215a00d69c562f73e51
Eduard <ylane9@live.fr>
2022-08-03 18:43:16 +0200

    Calendar file, hours from 0 to 23, well fomated

---
+++ src/Calendar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hour-wrapper" v-for="i in 24" v-bind:key="i" :id="'hour'+ (i-1)">
+    <h1>{{i - 1}}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+  
+</script>
+
+<style lang="scss" scoped>
+ .hour-wrapper {
+  height: 7%;
+  width: 90%;
+  background-color: whitesmoke;
+  border-radius: 2vh;
+  padding: 1vh;
+  border-left: 0.5vh solid black;
+  border-right: 0.5vh solid black;
+  border-top: 0.25vh solid black;
+  border-bottom: 0.25vh solid black;
+ }
+ #hour0 {
+  margin-top: 1%;
+  border-top: 0.5vh solid black;
+ }
+ #hour23 {
+  margin-bottom: 1%;
+  border-bottom: 0.5vh solid black;
+ }
+</style>
