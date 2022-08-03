d1268f567cdf8e5dd514412358c7358fb9291a94
Eduard <ylane9@live.fr>
2022-08-03 18:45:17 +0200

    Structure of the project done, with some style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <template>
+  <div id="all-wrapper">
+    <section id="calendar-wrapper">
+      <Calendar></Calendar>
+    </section>
+    <section id="tasks-wrapper">
+      <AddTask></AddTask>
+      <Tasks></Tasks>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import Calendar from "./Calendar.vue"
+  import AddTask from "./AddTask.vue"
+  import Tasks from "./Tasks.vue"
 </script>
 
 <style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #all-wrapper {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: blue;
 
+    #calendar-wrapper {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100%;
+      width: 70%;
+      background-color: red;
+      overflow-y: scroll;
+    }
+    #tasks-wrapper {
+      height: 100%;
+      width: 30%;
+      display: flex;
+      flex-direction: column;
+      background-color: green;
+    }
+  }
 </style>
