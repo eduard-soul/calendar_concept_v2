ed07c03b9e4233babf99603b543a60647944dae2
Eduard <ylane9@live.fr>
2022-08-03 18:43:47 +0200

    Tasks wrapper, well formated

---
+++ src/Tasks.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="tasks">
+    <div v-for="(task, n) in tasks_array" v-bind:key="n" :id="'task'+ n" class="task">
+      {{task.name}} {{n}}
+      {{task.duration}}
+      <button @click="deleteTask(n)">X</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { reactive } from 'vue';
+  
+  interface Task_object {
+    name: string,
+    duration: number,
+  }
+  let tasks_array:Array<Task_object> = reactive([]);
+
+  let task = {name: "Task", duration: 120};
+
+  tasks_array.push(task);
+
+  function deleteTask(n: number) {
+    tasks_array.splice(n, 1);
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  #tasks {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: yellow;
+  }
+  .task {
+    width: 90%; 
+    height: auto;
+    background-color: greenyellow;
+  }
+</style>
