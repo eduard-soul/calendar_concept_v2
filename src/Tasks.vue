328a06ae57e4f920af8475e85d47e2c86889955d
Eduard <ylane9@live.fr>
2022-08-09 11:59:10 +0200

    prettier and task now take the height of her duration when on calendar

--- src/Tasks.vue
+++ src/Tasks.vue
@@ -46,6 +46,7 @@ task_provided = inject("task_provide");
 
 function moveTask(e: MouseEvent) {
   let task = document.getElementById("task" + task_index.value);
+  let hours = document.getElementsByClassName('hour-wrapper');
 
   if (task) {
     if (first.value == true) {
@@ -55,6 +56,9 @@ function moveTask(e: MouseEvent) {
     }
     task.style.top = `${e.pageY - mouse_on_task.value.y}px`;
     task.style.left = `${e.pageX - mouse_on_task.value.x}px`;
+    if (isTasksOnCalendar(task_index.value)) {
+      task.style.height = `${(hours[0].getBoundingClientRect().height / 60) * tasks_array[task_index.value].duration}px`;
+    }
   }
 }
 
@@ -73,59 +77,81 @@ function taskDrag(n: number, mouse: boolean) {
 }
 
 function isTasksOnCalendar(n: number) {
-  let hours = document.getElementsByClassName('hour-wrapper');
-  let calendar_wrapper = document.getElementById('calendar-wrapper');
-  let task = document.getElementById('task'+n);
+  let hours = document.getElementsByClassName("hour-wrapper");
+  let calendar_wrapper = document.getElementById("calendar-wrapper");
+  let task = document.getElementById("task" + n);
 
   if (hours && calendar_wrapper && task) {
-    if (task.getBoundingClientRect().top >= hours[0].getBoundingClientRect().top
-      && task.getBoundingClientRect().top <= hours[23].getBoundingClientRect().bottom
-      && task.getBoundingClientRect().left >= hours[0].getBoundingClientRect().left
-      && task.getBoundingClientRect().left <= hours[0].getBoundingClientRect().right)   {
-      return (1);
-    }
-    else {
-      return (0);
+    if (
+      task.getBoundingClientRect().top >=
+        hours[0].getBoundingClientRect().top &&
+      task.getBoundingClientRect().top <=
+        hours[23].getBoundingClientRect().bottom &&
+      task.getBoundingClientRect().left >=
+        hours[0].getBoundingClientRect().left &&
+      task.getBoundingClientRect().left <=
+        hours[0].getBoundingClientRect().right
+    ) {
+      return 1;
+    } else {
+      task.style.height = `auto`;
+      return 0;
     }
   }
 }
 
 function whereIsTask(n: number) {
-  let where = {hour: 0, quarter: 0};
-  let task = document.getElementById('task'+n);
-  let hours = Array.from(document.getElementsByClassName('hour-wrapper') as HTMLCollectionOf<HTMLElement>); 
+  let where = { hour: 0, quarter: 0 };
+  let task = document.getElementById("task" + n);
+  let hours = Array.from(
+    document.getElementsByClassName(
+      "hour-wrapper"
+    ) as HTMLCollectionOf<HTMLElement>
+  );
 
   if (task) {
     let i = 0;
-    if (task.getBoundingClientRect().top >= hours[12].getBoundingClientRect().top) {
+    if (
+      task.getBoundingClientRect().top >= hours[12].getBoundingClientRect().top
+    ) {
       i = 12;
     }
     for (i; i < hours.length; i++) {
-      if (task.getBoundingClientRect().top <= hours[i].getBoundingClientRect().top) {
-        break ;
+      if (
+        task.getBoundingClientRect().top <= hours[i].getBoundingClientRect().top
+      ) {
+        break;
       }
     }
     i--;
     where.hour = i;
-    where.quarter = Math.floor((task.getBoundingClientRect().top - hours[i].getBoundingClientRect().top) / (hours[i].getBoundingClientRect().height / 4));
+    where.quarter = Math.floor(
+      (task.getBoundingClientRect().top -
+        hours[i].getBoundingClientRect().top) /
+        (hours[i].getBoundingClientRect().height / 4)
+    );
   }
 
-  return (where);
+  return where;
 }
 
-function putTaskOnQuarter(n: number, where: {hour: number, quarter: number}) {
-  let quarter = document.getElementById('h'+where.hour+'quarter'+where.quarter);
-  let task = document.getElementById('task'+n)
+function putTaskOnQuarter(n: number, where: { hour: number; quarter: number }) {
+  let quarter = document.getElementById(
+    "h" + where.hour + "quarter" + where.quarter
+  );
+  let task = document.getElementById("task" + n);
 
   if (quarter && task) {
     task.style.top = `${quarter.getBoundingClientRect().top}px`;
     task.style.left = `${quarter.getBoundingClientRect().left}px`;
-  } 
+  }
 }
 
 function applyPositionToTask() {
-  let tasks = Array.from(document.getElementsByClassName('task') as HTMLCollectionOf<HTMLElement>);
-  let input = document.getElementById('input-wrapper');
+  let tasks = Array.from(
+    document.getElementsByClassName("task") as HTMLCollectionOf<HTMLElement>
+  );
+  let input = document.getElementById("input-wrapper");
   let not_on_calendar_height = 0;
 
   if (tasks && input) {
@@ -136,14 +162,12 @@ function applyPositionToTask() {
       if (!isTasksOnCalendar(i)) {
         if (not_on_calendar_height !== 0) {
           tasks[i].style.top = `${not_on_calendar_height + input_height}px`;
-        }
-        else {
+        } else {
           tasks[i].style.top = `${input_height}px`;
         }
         tasks[i].style.left = `${input_left}px`;
         not_on_calendar_height += tasks[i].getBoundingClientRect().height;
-      } 
-      else {
+      } else {
         putTaskOnQuarter(i, whereIsTask(i));
         // console.log(whereIsTask(i));
       }
@@ -152,31 +176,42 @@ function applyPositionToTask() {
 }
 
 function checkScroll() {
-  let calendar = document.getElementById('calendar-wrapper');
-  let hours = document.getElementsByClassName('hour-wrapper');
+  let calendar = document.getElementById("calendar-wrapper");
+  let hours = document.getElementsByClassName("hour-wrapper");
   let task;
 
   if (calendar) {
-    calendar.addEventListener('scroll', () => {
+    calendar.addEventListener("scroll", () => {
       if (calendar) {
         if (previous_scroll.value == -1) {
           previous_scroll.value = calendar.scrollTop;
         }
         for (let i = 0; i < tasks_array.length; i++) {
-          task = document.getElementById('task'+i);
+          task = document.getElementById("task" + i);
 
           if (task && calendar) {
-            if (task.getBoundingClientRect().top - (calendar.scrollTop - previous_scroll.value) >= hours[0].getBoundingClientRect().top
-              && task.getBoundingClientRect().top - (calendar.scrollTop - previous_scroll.value)<= hours[23].getBoundingClientRect().bottom
-              && task.getBoundingClientRect().left >= hours[0].getBoundingClientRect().left
-              && task.getBoundingClientRect().left <= hours[0].getBoundingClientRect().right)   {
-                task.style.top = `${parseInt(task.style.top) - (calendar.scrollTop - previous_scroll.value)}px`;
-            }  
+            if (
+              task.getBoundingClientRect().top -
+                (calendar.scrollTop - previous_scroll.value) >=
+                hours[0].getBoundingClientRect().top &&
+              task.getBoundingClientRect().top -
+                (calendar.scrollTop - previous_scroll.value) <=
+                hours[23].getBoundingClientRect().bottom &&
+              task.getBoundingClientRect().left >=
+                hours[0].getBoundingClientRect().left &&
+              task.getBoundingClientRect().left <=
+                hours[0].getBoundingClientRect().right
+            ) {
+              task.style.top = `${
+                parseInt(task.style.top) -
+                (calendar.scrollTop - previous_scroll.value)
+              }px`;
+            }
           }
         }
         previous_scroll.value = calendar.scrollTop;
       }
-    })
+    });
   }
 }
 
